3508a5e14b992d2b111b5b24785b447f18ac95ff
Seunghwan8334 <seunghwan8334@gmail.com>
2022-12-07 22:45:38 +0900

    로그인과 인사말
    이름 적으면 인사를 해줌

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,16 @@
-//change test
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    console.log(username);
+    greeting.innerText = `hello ${username}`;  
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
